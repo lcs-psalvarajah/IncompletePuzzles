bedfd88259edbad7e38db4e8a3aba417df7a6391
jina <psalvarajah@lcs.on.ca>
2020-10-19 11:42:29 -0400

    Just added a guard feature in the code of daytime minutes, which means that if there are any human error when putting in number of minutes like "fifty" instead of an integer it should make the line repeat until the person types a integer.

--- ShiftySums/main.swift
+++ ShiftySums/main.swift
@@ -19,15 +19,47 @@ print("===========")
 let n = Int(readLine()!)!
 
 // How many times should we shift?
-let k = 3
+//    let k = 3
+
+let k = Int(readLine()!)!
 
 // PROCESS
 
-// Shift the given number "k" number of times
-var output = n
-output += n * 10
-output += n * 10 * 10
-output += n * 10 * 10 * 10
+    // Shift the given number "k" number of times
+var output = 0  // (12)
+    //output += n * 10   (120)       (132)
+    //output += n * 10 * 10    (1200)       (1332)
+    //output += n * 10 * 10 * 10     (12000)       (13332)
+
+//    var t: Int = 10
+//    var power: Int = k
+//
+//
+// for _ in 0...k {
+//
+//        output += n * Int(pow(Double(t), Double(power)))
+//
+//
+//}
+
+//infix operator **
+//func **(lhs: Double, rhs: Double) -> Double {
+//    return pow(lhs, rhs)
+//}
+
+
+let numberLoops = 0...k
+
+for i in numberLoops {
+    output += n
+    
+    for j in 1...k {
+        
+        
+    }
+    
+}
+
 
 // OUTPUT
 print(output)
--- TheCellSell/main.swift
+++ TheCellSell/main.swift
@@ -15,9 +15,40 @@ print("=============")
 
 // INPUT
 
-// Get daytime minutes
-print("Number of daytime minutes?")
-let day = Double(readLine()!)!
+//// Get daytime minutes
+//print("Number of daytime minutes?")
+//let day = Double(readLine()!)!
+
+
+//Get daytime minutes
+var day = 0
+while true {
+    
+    //Prompt the user
+    print("Number of daytime minutes?")
+    
+    //Collect input
+    guard let givenInput = readLine() else{
+        continue
+    }
+    
+    //Convert to an integer
+    guard let givenInteger = Int(givenInput) else {
+        continue
+    }
+    
+    //Check that the value is positive
+    guard givenInteger > 0 else {
+        continue
+    }
+    
+    //Assign the given integer to our "day" variable
+    day = givenInteger
+    
+    //Stop loop
+    break
+}
+    
 
 // Get evening minutes
 print("Number of evening minutes?")
