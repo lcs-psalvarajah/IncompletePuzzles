cf83854d67a2c6c2cbf8b3e2a718f910491a8349
jina <psalvarajah@lcs.on.ca>
2020-10-28 14:34:29 -0400

    Still working on the //process part of the ChipsFastFood puzzle. Right now I chose dictionary as my data collection, however arrays could still be a possibility.

--- ChipsFastFood/main.swift
+++ ChipsFastFood/main.swift
@@ -32,13 +32,47 @@ print("Welcome to Chip's Fast Food Emporium")
 //       Then drag and drop Extensions.swift to the Compile Sources subsection.
 //
 let burger = Int.collectInput(withPrompt: "Please enter a burger choice:", minimum: 1, maximum: 4)
-let drink = 3
-let side = 4
-let dessert = 1
+let drink = Int.collectInput(withPrompt: "Please enter a drink choice:", minimum: 1, maximum: 4)
+let side = Int.collectInput(withPrompt: "Please enter a side order choice:", minimum: 1, maximum: 4)
+let dessert = Int.collectInput(withPrompt: "Please enter a dessert choice", minimum: 1, maximum: 4)
 
 // PROCESS
 // NOTE: Give some thought to what kind of data structure would be helpful for this program.
 //       Perhaps a collection type of some sort?
+var output = 0
+    
+// A dictiory to store the information for burger
+
+var burgerOrder = [
+    1: 461,
+    2: 431,
+    3: 420,
+    4: 0
+]
+
+var drinkOrder = [
+    1: 130,
+    2: 160,
+    3: 118,
+    4: 0
+]
+
+var sideOrder = [
+    1: 100,
+    2: 57,
+    3: 70,
+    4: 0
+]
+
+var dessertOrder = [
+    1: 167,
+    2: 226,
+    3: 75,
+    4: 0]
+
+
 
 // OUTPUT
 // Show the calories total for the choices made by the user.
+
+print("Your calorie amount is \(output)")
