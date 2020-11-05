5c8dc0d207ba31405d5505ff327f935d59f9cfc2
jina <psalvarajah@lcs.on.ca>
2020-11-05 13:34:10 -0500

    refined the divisor count function to find the correct number of divisors

--- RSANumbers/main.swift
+++ RSANumbers/main.swift
@@ -27,36 +27,37 @@ let lower = Int(readLine()!)!
 print("Enter upper limit of range")
 let upper = Int(readLine()!)!
 
-//// PROCESS
-//// Count the number of divisors in the given Number
-//func divisorCount(of givenNumber: Int) -> Int {
-//
-//    // 1 is always a divisor
-//    var divisorCount = 1
-//
-//    // Start looping from 2
-//    for i in 2...givenNumber {
-//
-//        // What is the remainder?
-//        let remainder = givenNumber % i
-//
-//        // DEBUG output...
-//        print("\(givenNumber) \\ \(i) has a remainder of \(remainder)")
-//
-//        // TODO: Add some code here... we need to keep track of how many divisors there are
-//        if remainder == 0 {
-//            divisorCount += 1
-//        }
-//
-//    }
-//
-//    // Return the count of divisors
-//    return divisorCount
-//
-//}
-//
-//// How many divisors does a number have?
-//let numberOfDivisors = divisorCount(of: 10)
+// PROCESS
+// Count the number of divisors in the given Number
+func divisorCount(of givenNumber: Int) -> Int {
+
+    // 1 is always a divisor, so is the given number
+    var divisorCount = 2
+
+    // Start looping from 2
+    for i in 2...givenNumber / 2 {
+
+        // What is the remainder?
+        let remainder = givenNumber % i
+
+        // DEBUG output...
+        print("\(givenNumber) \\ \(i) has a remainder of \(remainder)")
+
+        // TODO: Add some code here... we need to keep track of how many divisors there are
+        if remainder == 0 {
+            divisorCount += 1
+        }
+
+    }
+
+    // Return the count of divisors
+    return divisorCount
+
+}
+
+// How many divisors does a number have?
+let numberOfDivisors = divisorCount(of: 10)
+print("10 has \(numberOfDivisors) divisors")
 
 for i in lower...upper {
     print(i)
